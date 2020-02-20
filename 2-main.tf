4834a61280565720f68dfdd58167b1dc46cc1767
Prakash Manglanathan <pmnathan@gmail.com>
2020-02-20 13:33:00 -0500

    Update 2-main.tf

--- 2-main.tf
+++ 2-main.tf
@@ -3,30 +3,30 @@ module "ecclandingzone" {
   version = "1.0.0"
 
   aws_region = var.aws_region
-  cidr_block  = var.vpc_cidr_block
+  cidr_block  = "10.0.0.0/16"
   default_tags = var.default_tags
 }
 
 resource "aws_subnet" "bu1_services" {
   vpc_id     = module.ecclandingzone.vpc_id
-  cidr_block = var.bu1_services_subnet_cidr
+  cidr_block = "10.0.1.0/24"
   tags = var.default_tags
 }
 
 resource "aws_subnet" "bu1_database" {
   vpc_id     = module.ecclandingzone.vpc_id
-  cidr_block = var.bu1_database_subnet_cidr
+  cidr_block = "10.0.2.0/24"
   tags = var.default_tags
 }
 
 resource "aws_subnet" "bu2_services" {
   vpc_id     = module.ecclandingzone.vpc_id
-  cidr_block = var.bu2_services_subnet_cidr
+  cidr_block = "10.0.3.0/24"
   tags = var.default_tags
 }
 
 resource "aws_subnet" "bu2_database" {
   vpc_id     = module.ecclandingzone.vpc_id
-  cidr_block = var.bu2_database_subnet_cidr
+  cidr_block = "10.0.4.0/24"
   tags = var.default_tags
 }
