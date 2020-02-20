64f86128a93e8a2ff30d1930829e571fdd3871de
Prakash Manglanathan <pmnathan@gmail.com>
2020-02-20 10:43:10 -0500

    Update 2-main.tf

--- 2-main.tf
+++ 2-main.tf
@@ -19,14 +19,6 @@ resource "aws_subnet" "bu1_database" {
   tags = var.default_tags
 }
 
-resource "aws_db_subnet_group" "bu1_database_group" {
-  name       = "bu1_database"
-  subnet_ids = [aws_subnet.bu1_database.id]
-  tags = var.default_tags
-}
-
-
-
 resource "aws_subnet" "bu2_services" {
   vpc_id     = module.ecclandingzone.vpc_id
   cidr_block = var.bu2_services_subnet_cidr
