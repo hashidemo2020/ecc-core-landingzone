eaca6a079f715bcd11208d85361c6be8cd577af2
Prakash Manglanathan <pmnathan@gmail.com>
2020-02-20 09:55:21 -0500

    Update 2-main.tf

--- 2-main.tf
+++ 2-main.tf
@@ -2,7 +2,7 @@ module "ecclandingzone" {
   source  = "app.terraform.io/FMR_ECC_CORE/ecclandingzone/aws"
   version = "1.0.0"
 
-  aws_reegion = var.aws_region
+  aws_region = var.aws_region
   cidr_block  = var.vpc_cidr_block
   default_tags = var.default_tags
 }
