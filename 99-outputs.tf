721d789226c87ed203a81f316842be92047b6f82
Prakash Manglanathan <pmnathan@gmail.com>
2020-02-20 10:28:54 -0500

    Update 99-outputs.tf

--- 99-outputs.tf
+++ 99-outputs.tf
@@ -5,3 +5,19 @@ output "vpc_id" {
 output "vpc_cidr" {
   value = module.ecclandingzone.vpc_cidr
 }
+
+output "bu1_services_subnet_id" {
+  value = aws_subnet.bu1_services.id
+}
+
+output "bu1_database_subnet_id" {
+  value = aws_subnet.bu1_database.id
+}
+
+output "bu2_services_subnet_id" {
+  value = aws_subnet.bu2_services.id
+}
+
+output "bu2_database_subnet_id" {
+  value = aws_subnet.bu2_database.id
+}
