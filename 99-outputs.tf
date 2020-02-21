8a7e8b1361333db6c2adffc6b85a4753b7b80295
Prakash Manglanathan <pmnathan@gmail.com>
2020-02-21 10:13:55 -0500

    Update 99-outputs.tf

--- 99-outputs.tf
+++ 99-outputs.tf
@@ -26,11 +26,11 @@ output "bu2_database_subnet_id_v1" {
   value = aws_subnet.bu2_database.id
 }
 
-output "bu1_services_subnet_id_v2" {
-  value = aws_subnet.bu1_services_v2.id
-}
+#output "bu1_services_subnet_id_v2" {
+#  value = aws_subnet.bu1_services_v2.id
+#}
 
-output "bu1_database_subnet_id_v2" {
-  value = aws_subnet.bu1_database_v2.id
-}
+#output "bu1_database_subnet_id_v2" {
+#  value = aws_subnet.bu1_database_v2.id
+#}
 
