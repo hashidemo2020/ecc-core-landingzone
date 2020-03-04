8a71cd034f49afe880a45316ed76082d0f91d9bd
WhatsARanjit <whatsaranjit@gmail.com>
2020-03-04 09:55:53 -0500

    Adding a new SG

--- 3-securitygroups.tf
+++ 3-securitygroups.tf
@@ -1,1 +1,20 @@
-#
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = module.ecclandingzone.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.1.0/24"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = ["pl-12c4e678"]
+  }
+}
