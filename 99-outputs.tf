4ae62dd0475b6c6da5390a6b8b3ac866242a27c1
WhatsARanjit <whatsaranjit@gmail.com>
2020-03-05 10:15:44 -0500

    Queueing up BU2 subnets

--- 99-outputs.tf
+++ 99-outputs.tf
@@ -13,3 +13,11 @@ output "bu1_services_subnet_id" {
 output "bu1_database_subnet_id" {
   value = aws_subnet.bu1_database.id
 }
+
+#output "bu2_services_subnet_id" {
+#  value = aws_subnet.bu2_services.id
+#}
+#
+#output "bu2_database_subnet_id" {
+#  value = aws_subnet.bu2_database.id
+#}
